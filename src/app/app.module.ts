e06fcc1c01b2e20a0457cc43088aaa386b22f9b1
Nahom Demessie <nahomnegdem@gmail.com>
2022-11-09 17:42:11 -0600

    header footer and dance components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {RouterModule} from "@angular/router";
+import { DanceListComponent } from './components/dance-list/dance-list.component';
+import { DanceDetailComponent } from './components/dance-detail/dance-detail.component';
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DanceListComponent,
+    DanceDetailComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot([
+          {
+            path:'',
+            component:DanceListComponent
+          },
+          {
+            path:'dances/:danceId',
+            component:DanceDetailComponent
+          }
+        ])
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
