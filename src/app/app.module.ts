483dc39c58da325831bcdeacb14dff652e3e76ea
Nahom Demessie <nahomnegdem@gmail.com>
2022-11-17 21:05:21 -0600

    ant design integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,15 @@ import {HttpClientModule} from "@angular/common/http";
 import {AddDanceComponent} from "./components/add-dance/add-dance.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { LoginComponent } from './components/login/login.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzCardModule} from "ng-zorro-antd/card";
+
+registerLocaleData(en);
 
 
 @NgModule({
@@ -23,35 +32,40 @@ import { LoginComponent } from './components/login/login.component';
     DanceDetailComponent,
     LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        component: DanceListComponent
-      },
-      {
-        path: 'dances/:danceId',
-        component: DanceDetailComponent
-      },
-      {
-        path: 'adddance',
-        component : AddDanceComponent
-      },
-      {
-        path: 'login',
-        component :LoginComponent
-      },
-      {
-        path:'**',
-        component: DanceListComponent
-      }
-    ]),
-    FormsModule,
-    ReactiveFormsModule
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                component: DanceListComponent
+            },
+            {
+                path: 'dances/:danceId',
+                component: DanceDetailComponent
+            },
+            {
+                path: 'adddance',
+                component: AddDanceComponent
+            },
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: '**',
+                component: DanceListComponent
+            }
+        ]),
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        NzButtonModule,
+        NzCardModule
+    ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
